599a5b9f824ed977088afa323bb9fd2c06f116d2
Muhammad Tehami <t3hami@gmail.com>
2022-06-29 02:47:36 +0500

    init

--- vars/sample.groovy
+++ vars/sample.groovy
@@ -1,11 +1,7 @@
 import com.nisum.devops.Sample
 
+@NonCPS
 def call(body) {
-    @NonCPS
-    stage('Sample') {
-        script {
-            def sampleObj = new Sample(1, 2)
-            println sampleObj
-        }
-    }
+    def sampleObj = new Sample()
+    println sampleObj
 }
